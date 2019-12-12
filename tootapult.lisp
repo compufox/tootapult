9693fc8dcc6698e19185995dc44bee8be98aebbe
ava fox <dev@computerfox.xyz>
2019-12-12 18:12:48 -0500

    added toot privacy and reply checks
    added saving/loading id maps
    updated example config with more comments
    added error catching on fetching mastodon account id
    fixed adding cw into posts

--- tootapult.lisp
+++ tootapult.lisp
@@ -2,19 +2,23 @@
 
 (in-package #:tootapult)
 
-(defvar *max-tweet-length* 280)
+(defconstant *max-tweet-length* 280)
+(defconstant *privacy-values* '("direct" "private" "unlisted" "public"))
+(defconstant *map-filename* "posts.map")
+
 (defvar *mastodon-instance*)
-(defvar *mastodon-account*)
+(defvar *mastodon-account-id*)
 (defvar *mastodon-token*)
 (defvar *id-mappings*)
 (defvar *filters*)
-(defvar *privacy-level* "public")
-(defvar *privacy-values* '("direct" "private" "unlisted" "public"))
+(defvar *privacy-level*)
 (defvar *crosspost-mentions*)
 
 (defun main ()
   "binary entry point"
   (load-config)
+  (import-id-map)
+  
   (handler-case
       (with-user-abort (start-crossposter))
     (user-abort ()
@@ -22,7 +26,9 @@
       (uiop:quit))
     (error (e)
       (format t "encountered error ~a~%" e)
-      (uiop:quit 1))))
+      (uiop:quit 1)))
+
+  (export-id-map))
 
 (defun load-config ()
   "loads our config file and sets our variables accordingly"
@@ -30,23 +36,28 @@
 				"tootapult.config"))))
     (setf *mastodon-instance* (agetf config "mastodon-url")
 	  *mastodon-token* (agetf config "mastodon-token")
-	  *mastodon-account* (get-account)
+	  *mastodon-account-id* (get-mastodon-account-id)
 	  *oauth-api-key* (agetf config "twitter-consumer-key")
 	  *oauth-api-secret* (agetf config "twitter-consumer-secret")
 	  *oauth-access-token* (agetf config "twitter-access-token")
 	  *oauth-access-secret* (agetf config "twitter-token-secret")
 
+	  *privacy-level* (member (agetf config "privacy-level" '("public")) *privacy-values*
+				  :test #'string=)
 	  *filters* (mapcar #'str:trim (split #\, (agetf config "filters")))
 	  *crosspost-mentions* (agetf config "crosspost-mentioned"))))
 
-(defun get-account ()
+(defun get-mastodon-account-id ()
   "fetches the account that belongs to the token"
-  (decode-json-from-string
-   (dex:get (format nil "https://~a/api/v1/accounts/verify_credentials"
-		    (replace-all "https://" "" *mastodon-instance*))
-	    :headers `(("Authorization" . ,(concatenate 'string
-							"Bearer "
-							*mastodon-token*))))))
+  (handler-case 
+      (agetf (decode-json-from-string
+	      (dex:get (format nil "https://~a/api/v1/accounts/verify_credentials"
+			       (replace-all "https://" "" *mastodon-instance*))
+		       :headers `(("Authorization" . ,(concatenate 'string
+								   "Bearer "
+								   *mastodon-token*)))))
+	     :id)
+    (error (e) (error "incorrect mastodon token"))))
 
 (defun parse-file (file)
   "parses FILE, returning an alist"
@@ -72,7 +83,7 @@
 (defun start-crossposter ()
   "starts streaming from the mastodon endpoint, handling new events as the arise"
   (unless (chirp:account/verify-credentials)
-    (error "incorrect credentials"))
+    (error "incorrect twitter credentials"))
 
   (loop
      with masto-stream = (dex:get (format nil "https://~a/api/v1/streaming/user"
@@ -102,8 +113,10 @@ data: json post object, status id, json notification object
       ;;    and its not a reblog
       ;;  crosspost it
       ((and (string= event-type "update")
-	    (equal (agetf parsed-data :account) *mastodon-account*)
-	    (null (agetf parsed-data :reblog)))
+	    (equal (agetf (agetf parsed-data :account) :id) *mastodon-account-id*)
+	    (null (agetf parsed-data :reblog))
+	    (self-reply-p status)
+	    (should-crosspost-p status))
        (post-to-twitter (decode-json-from-string data)))
 
       ;; if its a delete event, and we have the id stored somewhere
@@ -120,43 +133,54 @@ data: json post object, status id, json notification object
 (defun post-to-twitter (status)
   ;; this loop macro is bad and i feel bad writing it.
   ;; should be split into PROPER lisp code lol
-    
-  (when (should-crosspost-p status)
-    
-      ;; should also add cw's please. do not forget
-    (loop
-       with tweet-length = 0
-       with tweet-words = (words (build-post status))
-       with tweet = nil
-       with last-id = nil
-       with media-list = (get-post-media (agetf status :media-attachments))
-	 
-       while tweet-words
-       do
-	 (push (pop tweet-words) tweet)
-	 (setf tweet-length (chirp:compute-status-length (join #\Space tweet)))
-	 
-       when (> tweet-length *max-tweet-length*)
-       do
-	 (push (pop tweet) tweet-words)
-	 (setf last-id (slot-value (chirp:tweet (join #\Space (reverse tweet))
-						:reply-to last-id
-						:file media-list)
-				   'chirp::%id)
-	       media-list nil
-	       tweet nil
-	       *id-mappings* (append *id-mappings*
-				     (cons (agetf status :id)
-					   last-id)))
-				     
-       finally
-	 (chirp:statuses/update (join #\Space (reverse tweet))))))
+  (loop
+     with tweet-length = 0
+     with tweet-words = (words (build-post status))
+     with tweet = nil
+     with last-id = (cdr (find (agetf status :id) *id-mappings* :key #'car :from-end t))
+     with media-list = (get-post-media (agetf status :media-attachments))
+       
+     while tweet-words
+     do
+       (push (pop tweet-words) tweet)
+       (setf tweet-length (chirp:compute-status-length (join #\Space tweet)))
+       
+     when (> tweet-length *max-tweet-length*)
+     do
+       (push (pop tweet) tweet-words)
+       (setf last-id (slot-value (chirp:tweet (join #\Space (reverse tweet))
+					      :reply-to last-id
+					      :file media-list)
+				 'chirp::%id)
+	     media-list nil
+	     tweet nil
+	     *id-mappings* (append *id-mappings*
+				   (cons (agetf status :id)
+					 last-id)))
+       
+     finally
+       (when tweet
+	 (chirp:tweet (join #\Space (reverse tweet))
+		      :reply-to last-id
+		      :file media-list))))
+
+(defun self-reply-p (status)
+  "checks if STATUS is a self-reply"
+  (let ((is-reply (agetf status :in-reply-to-id)))
+    (cond
+      ((and is-reply
+	    (equal (agetf status :in-reply-to-account-id) *mastodon-account-id*))
+       t)
+      ((not is-reply)
+       t)
+      (t nil))))
 
 (defun should-crosspost-p (status)
   "checks if we should crosspost the status"
   (let ((filtered (filter-present-p content))
 	(mentions (agetf status :mentions)))
-    (and (or *crosspost-mentions* (null mentions))
+    (and (member (agetf status :visibility) *privacy-level* :test #'string=)
+	 (or *crosspost-mentions* (null mentions))
 	 (not filtered))))
 
 (defun filter-present-p (status-text)
@@ -185,7 +209,7 @@ replaces mentions, if specified"
 		 (replace-all handle (agetf m :url) content))))
       
     (concatenate 'string
-		 cw (when cw (format nil ":~&~&"))
+		 (when cw (format nil "cw: ~A~&~&" cw))
 		 (if *crosspost-mentions*
 		     (mapcar #'replace-mention mentions)
 		     content)))))
@@ -222,3 +246,20 @@ returns the filename"
 
     ;; and them remove them from our mappings
     (setf *id-mappings* (remove id *id-mappings* :test #'= :key #'car))))
+
+(defun import-id-map ()
+  "reads the mappings and sets *ID-MAPPINGS*"
+  (with-open-file (in *map-filename*
+		      :direction :input
+		      :if-does-not-exist nil)
+    (when in
+      (setf *id-mappings*
+	    (read-from-string (read-line in nil nil))))))
+
+(defun export-id-map ()
+  "saves our mappings for later"
+  (with-open-file (out *map-filename*
+		       :direction :output
+		       :if-exists :overwrite
+		       :if-does-not-exist :create)
+    (write-string (format nil "~s" *id-mappings*) out)))
