642fedcd76ac2a77d2188813babf0ccd623542a2
ava fox <dev@computerfox.xyz>
2020-01-18 18:27:11 -0500

    moved all functions relating to posts into posts.lisp
    updated system definition

--- tootapult.asd
+++ tootapult.asd
@@ -14,11 +14,11 @@
 	       #:tooter)
   :components ((:file "package")
 	       (:file "util")
+	       (:file "posts")
                (:file "tootapult"))
   :build-operation "program-op"
   :build-pathname "bin/tootapult"
   :entry-point "tootapult::main")
-
 
 #+sb-core-compression
 (defmethod asdf:perform ((o asdf:image-op) (c asdf:system))
--- util.lisp
+++ util.lisp
@@ -96,71 +96,6 @@
   (or (cdr (assoc indicator place :test #'equal))
       default))
 
-(defun get-words (text)
-  "properly splits a toot up into words, preserving newlines"
-  (reverse
-    (loop
-       with words = ()
-       with cur-start = 0
-				   
-       if (< cur-start (length text))
-       do
-	 (let ((s (search " " text :test #'string= :start2 cur-start)))
-	   (setf cur-start
-		 (if s
-		     (prog1 (1+ s)
-		       (push (str:substring cur-start s text) words))
-		     (prog1 (length text)
-		       (push (str:substring cur-start (length text) text)
-			     words)))))
-       else
-       return words)))
-
-(defun self-reply-p (status)
-  "checks if STATUS is a self-reply"
-  (let ((is-reply (agetf status :in--reply--to--id)))
-    (cond
-      ((and is-reply (equal (agetf status :in--reply--to--account--id)
-			    *mastodon-account-id*))
-       t)
-      ((not is-reply) t)
-      (t nil))))
-
-(defun should-crosspost-p (status)
-  "checks if we should crosspost the status"
-  (let ((filtered (or (filter-present-p (agetf status :content))
-		      (filter-present-p (agetf status :spoiler--text))))
-	(mentions (agetf status :mentions))
-	(is-reply (agetf status :in--reply--to--id)))
-    (and (member (agetf status :visibility) *privacy-level* :test #'string=)
-	 (or *crosspost-mentions* (null mentions))
-	 (not filtered)
-	 (or (not is-reply)
-	     (self-reply-p status)))))
-
-(defun filter-present-p (status-text)
-  "checks if any filter words appear in STATUS-TEXT"
-  (loop
-     for f in *filters*
-
-     when (containsp f status-text :ignore-case t)
-     return t))
-
-(defun sanitize-content (post)
-  "removes all html tags from POST
-
-returns a list of each newline-separated paragraph"
-  (remove-if #'blankp
-	     (flatten
-	      (loop
-		 with content
-		 for c across (plump:children (plump:parse (html-entities:decode-entities post)))
-		 do (push (loop
-			   for c1 across (plump:children c)
-			   collect (plump:text c1))
-			  content)
-		 finally (return (reverse content))))))
-
 (defun import-id-map ()
   "reads the mappings and sets *ID-MAPPINGS*"
   (with-open-file (in *map-filename*
@@ -195,15 +130,6 @@ returns a list of each newline-separated paragraph"
 		      
    when (equal key toot-id)
    collect value))
-
-(defun replace-all-mentions (mentions content)
-  "replaces all @mentions in CONTENT with URL to account"
-  (let ((fixed-content content))
-    (dolist (mtn mentions fixed-content)
-      (setf fixed-content
-	    (replace-all (concatenate 'string "@" (first (split #\@ (agetf mtn :acct))))
-			 (agetf mtn :url)
-			 fixed-content)))))
 
 (defun authenticate-twitter ()
   "opens the twitter authentication prompt"
